e505eb70d2a61b44b9d14f32314a3ec0cf2935b4
Aman Tomar <82502153+ChaudharyAman@users.noreply.github.com>
2023-02-09 22:28:02 +0530

    Create pg5.java

---
+++ pg5.java
@@ -0,0 +1,16 @@
+class A{
+    public void methodA(){
+        System.out.println("A");
+    }
+}
+class B extends A{
+    public void methodB() {
+        System.out.println("B");
+    }
+}
+//Aman Tomar
+class C extends B{
+    public void methodC() {
+        System.out.println("C");
+    }
+}
